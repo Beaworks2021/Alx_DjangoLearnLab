65e1b2534bd55362a08e32868d198e6051ce30e8
Beaworks2021 <ekowworks@gmail.com>
2025-07-20 23:51:24 +0000

    update code

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, home, books_by_author, LibraryListView
+from .views import list_books, LibraryDetailView
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    # Home page
-    path('', home, name='home'),
-    
     # Function-based view for listing all books
     path('books/', list_books, name='list_books'),
     
     # Class-based view for library detail
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    
-    # Additional views for better functionality
-    path('libraries/', LibraryListView.as_view(), name='library_list'),
-    path('author/<int:author_id>/books/', books_by_author, name='author_books'),
 ]
